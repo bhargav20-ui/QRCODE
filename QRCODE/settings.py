af189a4a1cf2a6b7c2115e61adc24c744cb922bc
Bhargav <nakkabhargav20@gmail.com>
2025-12-26 11:45:38 +0530

    Add build script for deployment

--- QRCODE/settings.py
+++ QRCODE/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,12 +26,8 @@ SECRET_KEY = 'django-insecure-@71n17lzog7!5_b$$jwmujz=v-(#9o_=3@9a(%61oh9+^xxds9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "*",
-    "127.0.0.1",
-    "localhost",
-    "wisplike-hypolimnial-sharonda.ngrok-free.dev",
-]
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
@@ -126,7 +123,9 @@ CSRF_TRUSTED_ORIGINS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
